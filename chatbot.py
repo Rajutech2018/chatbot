38126c30ead99ad5c26f724a2b53b5a0b9ab52af
Rajutech2018 <161584379+Rajutech2018@users.noreply.github.com>
2024-04-29 22:44:01 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,7 @@ if 'buffer_memory' not in st.session_state:
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Welcome to Raju ChatBot, How can I help you today?"}
+        {"role": "assistant", "content": "How can I help you today?"}
     ]
 
 # Initialize ChatOpenAI and ConversationChain
@@ -30,7 +30,7 @@ conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
 
 # Create user interface
 st.title("🗣️ Conversational Chatbot")
-st.subheader("㈻ Simple Chat Interface for LLMs")
+st.subheader("Welcome to Raju ChatBot,")
 
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
